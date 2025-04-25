378982a6dd8e2fea465e7e4668e4e5dc93dd3824
Rubén Sánchez <rsanchezm110@gmail.com>
2025-04-25 21:53:01 +0200

    useclient/server - usepathname

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,6 +9,9 @@ export default function Layout (
             <div className="w-full flex-none md:w-64">
                 <SideNav/>
             </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </div>
         </div>
     );
 }
